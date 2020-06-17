40c0f89f2a77b202158bf6e9e42950df841a6efd
Chad Michel <chadmichel@me.com>
2020-06-17 11:01:45 -0500

    make an editable form

--- PMToolClient/src/app/app.module.ts
+++ PMToolClient/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './pages/test/test.component';
 import { PlanComponent } from './pages/plan/plan.component';
+import { FormsModule }   from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { PlanComponent } from './pages/plan/plan.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
